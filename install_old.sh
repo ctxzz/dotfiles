29dc1fc282f3f62d23f60979b7b0781533f53ea3
ユーザ名 <ctxs19@gmail.com>
2024-06-01 16:34:55 +0900

    remove latex

--- install_old.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-
-    ln -sfnv $(cd "$(dirname "$f")" && pwd)/$(basename "$f") $HOME/"$f";
-done
